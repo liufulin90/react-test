07e5488edf4c25a5e2319d405505575125cac004
liufulin90 <liufulin90@163.com>
2017-02-28 22:59:53 +0800

    update

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import {Provider} from 'react-redux'
 import {Router} from 'react-router'
 import routes from './routes/'
 import store, {history} from './redux/store/'
-import {getRandomInt} from 'js-common-tools'
+import * as JST from 'js-common-tools'
 
 const mountNode = document.getElementById('app')
-console.log(getRandomInt())
+let testKey = 'jjj'
+JST.setLocalStorage(testKey, 'test names')
+console.log(JST.getLocalStorage(testKey))
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history} children={routes}/>
